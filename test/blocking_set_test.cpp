446a3fb328d8f002a3cc5b45832d1cff77f6e013
igorous <iamiggor@gmail.com>
2017-10-22 22:30:35 +0300

    added tests for blocking set

---
+++ test/blocking_set_test.cpp
@@ -0,0 +1,21 @@
+#include "gtest/gtest.h"
+#include "blocking_set.h"
+
+using namespace std;
+using namespace NElevator;
+
+namespace {
+
+TEST(BlockingSetTest, Closest) {
+  BlockingSet<int> bset;
+  bset.insert(2);
+  bset.insert(5);
+  bset.insert(7);
+  bset.insert(10);
+  bset.insert(20);
+  ASSERT_EQ(bset.get_closest(8), 7);
+  ASSERT_EQ(bset.get_closest(22), 20);
+  ASSERT_EQ(bset.get_closest(1), 2);
+}
+
+}
